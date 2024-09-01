d73d6df94773293e344dedab929866877e68e2a4
WKotlinski <132941585+WKotlinski@users.noreply.github.com>
2024-09-01 09:18:52 +0200

    adding findbyid and fixing bcript

--- controllers/users_services.js
+++ controllers/users_services.js
@@ -1,20 +1,29 @@
 const { Users } = require("../models/user");
+const bcript = require("bcrypt");
 
 const registerUser = async ({ email, password }) => {
-  const newUser = {
+  const newUser = new Users({
     email,
-    password: bcript.hash(password),
-  };
+    password: await bcript.hash(password, 10),
+  });
+  await newUser.save();
+  return newUser;
 };
 
-const loginUser = async () => {};
 const findUser = async ({ email }) => {
   const user = Users.findOne({ email });
   if (user) throw new Error("Email already taken");
   return user;
 };
+const findById = async ({ id }) => {
+  const user = Users.findById({ id });
+  return user;
+};
+
+const loginUser = async () => {};
 module.exports = {
   findUser,
+  findById,
   registerUser,
   loginUser,
 };
