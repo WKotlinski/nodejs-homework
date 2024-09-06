b8073d3a4feb1e9230599a32db90fe4e2072a190
WKotlinski <132941585+WKotlinski@users.noreply.github.com>
2024-09-06 11:24:48 +0200

    Creating email.js

---
+++ email.js
@@ -0,0 +1,27 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const config = {
+  host: "smtp.sendgrid.net",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "apikey",
+    pass: process.env.SENDGRID_API_KEY,
+  },
+};
+
+const transporter = nodemailer.createTransport(config);
+
+const emailOptions = (to, subject, html) => {
+  return { from: "Wojciech Kot <wojcio888@gmail.com>", to, subject, html };
+};
+const sendMail = async (to, subject, html) => {
+  try {
+    const info = await transporter.sendMail(emailOptions(to, subject, html));
+    console.log(info);
+  } catch (e) {
+    console.log(e);
+  }
+};
+module.exports = { sendMail };
